26e75f59f154bc230cf93e883bd02575f4d14531
Shukan Shah <shukan@Shukans-MBP.home>
2020-05-18 03:05:22 -0400

    Updates

---
+++ precache-manifest.809c66cce01c702fb6f180920c293fca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb8eecd753918f48e13eb6ebb480f9c",
+    "url": "/arXived/index.html"
+  },
+  {
+    "revision": "8942d281aab0173d51cc",
+    "url": "/arXived/static/css/main.167f84fb.chunk.css"
+  },
+  {
+    "revision": "38095659379d1483a1d1",
+    "url": "/arXived/static/js/2.06104b62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/arXived/static/js/2.06104b62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8942d281aab0173d51cc",
+    "url": "/arXived/static/js/main.5e469926.chunk.js"
+  },
+  {
+    "revision": "ddb9b89b753c98fd4029",
+    "url": "/arXived/static/js/runtime-main.0a543eb4.js"
+  }
+]);
